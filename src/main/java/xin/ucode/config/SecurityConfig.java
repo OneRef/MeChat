92cca3786fc2588d99bfdb6ab1f400d4a415e96e
韦雨庭 <v2472331253@163.com>
2023-09-06 10:52:47 +0800

    fix: 解决了不上传token也能访问接口的问题

--- src/main/java/xin/ucode/config/SecurityConfig.java
+++ src/main/java/xin/ucode/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/user/login").permitAll()
                 //允许所有人访问knife4j
-                .antMatchers("/doc.html","/webjars/**","/img.icons/**","/swagger-resources/**","/**","/v2/api-docs").permitAll()
+                .antMatchers("/doc.html","/webjars/**","/img.icons/**","/swagger-resources/**","/v2/api-docs").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
         http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
